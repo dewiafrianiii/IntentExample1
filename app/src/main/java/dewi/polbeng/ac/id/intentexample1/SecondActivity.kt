0ad1716247d6450de3a84688b3651edcf5e38763
dewiafrianiii <afrianidewi551@gmail.com>
2024-10-14 15:27:31 +0700

    add commit.

---
+++ app/src/main/java/dewi/polbeng/ac/id/intentexample1/SecondActivity.kt
@@ -0,0 +1,28 @@
+package dewi.polbeng.ac.id.intentexample1
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import dewi.polbeng.ac.id.intentexample1.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.i("SecondActivity", "onCreate")
+        binding.btnExitActivity.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("SecondActivity", "onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("SecondActivity", "onStop")
+    }
+}
